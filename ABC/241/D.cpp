061ecff5981d504e5c2037e3b6b4943a1219153d
Siddik <absiddik718@gmail.com>
2022-05-01 14:50:15 +0600

    init

---
+++ ABC/241/D.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using ll = long long int;
+using ld = long double;
+using namespace std;
+void solve(){
+   
+}
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    solve();
+    return 0;
+}
